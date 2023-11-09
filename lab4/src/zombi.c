4641803cbc1d0922c3b8b7eb0ebbf0b52592c325
ineedkodeforlife <109513419+ineedkodeforlife@users.noreply.github.com>
2023-11-09 15:48:29 +0300

    add zombi

---
+++ lab4/src/zombi.c
@@ -0,0 +1,26 @@
+#include <sys/wait.h>  
+#include <stdlib.h>  
+#include <stdio.h>
+#include <unistd.h>  
+  
+int main(void)  
+{  
+    pid_t pids[10];  
+    int i;  
+  
+    for (i = 9; i >= 0; --i) {  
+        pids[i] = fork();  
+        if (pids[i] == 0) {  
+            printf("Child%d\n", i);  
+            sleep(i+1);  
+            _exit(0);  
+        }  
+    }  
+  
+    for (i = 9; i >= 0; --i) {  
+        printf("parent%d\n", i);  
+        waitpid(pids[i], NULL, 0);  
+    }  
+  
+    return 0;  
+}  
