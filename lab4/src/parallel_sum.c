5009ac43d9214911631c75a3f3760a88cfc76807
ineedkodeforlife <109513419+ineedkodeforlife@users.noreply.github.com>
2023-11-09 19:00:33 +0300

    Update parallel_sum.c

--- lab4/src/parallel_sum.c
+++ lab4/src/parallel_sum.c
@@ -1,8 +1,8 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
-
 #include <pthread.h>
+#include "utils.h"
 
 struct SumArgs {
   int *array;
@@ -12,39 +12,32 @@ struct SumArgs {
 
 int Sum(const struct SumArgs *args) {
   int sum = 0;
-  // TODO: your code here 
+  for (int i = args->begin; i < args->end; i++) {
+    sum += args->array[i];
+  }
   return sum;
 }
 
-void *ThreadSum(void *args) {
-  struct SumArgs *sum_args = (struct SumArgs *)args;
-  return (void *)(size_t)Sum(sum_args);
-}
-
 int main(int argc, char **argv) {
-  /*
-   *  TODO:
-   *  threads_num by command line arguments
-   *  array_size by command line arguments
-   *	seed by command line arguments
-   */
+  if (argc < 4) {
+    printf("Usage: %s <threads_num> <array_size> <seed>\n", argv[0]);
+    return 1;
+  }
 
-  uint32_t threads_num = 0;
-  uint32_t array_size = 0;
-  uint32_t seed = 0;
+  uint32_t threads_num = atoi(argv[1]);
+  uint32_t array_size = atoi(argv[2]);
+  uint32_t seed = atoi(argv[3]);
   pthread_t threads[threads_num];
-
-  /*
-   * TODO:
-   * your code here
-   * Generate array here
-   */
-
   int *array = malloc(sizeof(int) * array_size);
+  GenerateArray(array, array_size, seed);
 
   struct SumArgs args[threads_num];
+  int chunk_size = array_size / threads_num;
   for (uint32_t i = 0; i < threads_num; i++) {
-    if (pthread_create(&threads[i], NULL, ThreadSum, (void *)&args)) {
+    args[i].array = array;
+    args[i].begin = i * chunk_size;
+    args[i].end = (i == threads_num - 1) ? array_size : (i + 1) * chunk_size;
+    if (pthread_create(&threads[i], NULL, (void *(*)(void *))Sum, (void *)&args[i])) {
       printf("Error: pthread_create failed!\n");
       return 1;
     }
